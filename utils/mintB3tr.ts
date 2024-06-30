09a19bec89b9d6b4f3e953bd9613f111b357e745
Tom Rowbotham <tom.rowbotham@live.co.uk>
2024-06-30 01:38:11 -0700

    IT WORKS

--- utils/mintB3tr.ts
+++ utils/mintB3tr.ts
@@ -7,12 +7,18 @@ import {
   TESTNET_NETWORK,
   TransactionHandler,
   mnemonic,
-    
+  unitsUtils,
 } from "@vechain/sdk-core";
 
-import {ethers} from "ethers"
+import { ethers } from "ethers";
 
-import { ThorClient } from "@vechain/sdk-network";
+import {
+  ProviderInternalBaseWallet,
+  ThorClient,
+  VeChainProvider,
+  signerUtils,
+} from "@vechain/sdk-network";
+
 // First way to initialize thor client
 const testnetUrl = "https://testnet.vechain.org/";
 
@@ -41,73 +47,88 @@ const mitnABi = [
 ];
 
 export const mintB3tr = async (address: string, amount: string) => {
-    const parsedAmount = ethers.parseEther(amount);
-  // 2 - Create a clause to call setValue(123)
+  const parsedAmount = ethers.parseEther(amount);
   const clause = clauseBuilder.functionInteraction(
     "0x4b6883ee39447ea10c93e0ac9bd630a18d357f38", // just a sample deployed contract address
     coder.createInterface(mitnABi).getFunction("mint") as FunctionFragment,
     [address, parsedAmount]
   );
 
-  const clauses = [clause];
+  const _mnemonic =
+  "supreme position hammer gravity swim lawsuit pulp apart album assault ranch rich";
+
+  const mnemonicArray = _mnemonic.split(" ");
+
+  const privateKey = mnemonic.derivePrivateKey(mnemonicArray);
+  const privateKeyBuffer = Buffer.from(privateKey);
+
+  const provider = new VeChainProvider(
+    // Thor client used by the provider
+    thorClient,
+
+    // Internal wallet used by the provider (needed to call the getSigner() method)
+    new ProviderInternalBaseWallet([
+      {
+        privateKey: privateKeyBuffer,
+        address: address,
+      },
+    ]),
+
+    // Disable fee delegation (BY DEFAULT IT IS DISABLED)
+    false
+  );
+
 
   // 2 - Calculate intrinsic gas of clauses
+const transaction = {
+    clauses: [
+       clause
+    ],
+    simulateTransactionOptions: {
+        caller: address
+    }
+};
 
-  const gas = TransactionUtils.intrinsicGas(clauses);
 
-  // 2 - Get latest block
+  const gasResult = await thorClient.gas.estimateGas(
+    transaction.clauses,
+    transaction.simulateTransactionOptions.caller
+);
 
-const latestBlock = await thorClient.blocks.getBestBlockCompressed();
 
   // 3 - Body of transaction
 
-  const body: TransactionBody = {
-    chainTag: TESTNET_NETWORK.chainTag,
-    blockRef: latestBlock !== null ? latestBlock.id.slice(0, 18) : '0x0',
-    expiration: 0,
-    clauses,
-    gasPriceCoef: 128,
-    gas,
-    dependsOn: null,
-    nonce: 12345678,
-  };
+  const body = await thorClient.transactions.buildTransactionBody(
+    transaction.clauses,
+    gasResult.totalGas
+);
 
   // 4 - Sign transaction
-  const _mnemonic =
-    "supreme position hammer gravity swim lawsuit pulp apart album assault ranch rich";
-  const mnemonicArray = _mnemonic.split(" ");
 
   // Defined for VET at https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-  const privateKey = mnemonic.derivePrivateKey(mnemonicArray);
+ 
 
-  const signedTransaction = TransactionHandler.sign(
-    body,
-    Buffer.from(privateKey)
-  ).encoded;
+  const signer = await provider.getSigner(address);
 
-  
+  const rawSignedTransaction = await signer!!.signTransaction(
+    signerUtils.transactionBodyToTransactionRequestInput(body, address)
+  );
+
+  const signedTransaction = TransactionHandler.decode(
+    Buffer.from(rawSignedTransaction.slice(2), "hex"),
+    true
+  );
 
   // 6 - Send transaction
 
-  const send = await thorClient.transactions.sendRawTransaction(
-    `0x${signedTransaction.toString("hex")}`
+  const sendTransactionResult = await thorClient.transactions.sendTransaction(
+    signedTransaction
   );
 
-  // 7 - Get transaction details and receipt
-
-  const transactionDetails = await thorClient.transactions.getTransaction(
-    send.id
+  // 6 - Wait for transaction receipt
+  const txReceipt = await thorClient.transactions.waitForTransaction(
+    sendTransactionResult.id
   );
-  const transactionReceipt =
-    await thorClient.transactions.getTransactionReceipt(send.id);
 
-    console.log("transaction details", transactionDetails);
-    console.log("transaction receipt", transactionReceipt);
-    console.log("send", send);
-
-  return {
-    id: send.id,
-    details: transactionDetails,
-    receipt: transactionReceipt,
-  };
+  return txReceipt;
 };
